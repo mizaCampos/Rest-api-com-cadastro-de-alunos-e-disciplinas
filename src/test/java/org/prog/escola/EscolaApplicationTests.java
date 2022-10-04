e331c44856c85c89e7574870aae15a48a6e4fc92
Mizael Lauriano <102499938+mizaCampos@users.noreply.github.com>
2022-10-04 16:10:05 -0300

    Add files via upload

---
+++ src/test/java/org/prog/escola/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prog.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
